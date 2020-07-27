f042e6e38cc2cb927c32cff2fb748bfda9724b42
jacobtdev <astrojacob544@gmail.com>
2020-07-27 04:15:25 +0200

    added marker for stations

--- client/client.lua
+++ client/client.lua
@@ -14,6 +14,8 @@ local hasReceivedDepartmentID = false
 local isPolice = false
 local inService = false
 
+local policeStations = { vector3(1857.01, 3689.56, 34.2671) }
+
 -- Get current department
 AddEventHandler("onClientResourceStart", function()
     TriggerServerEvent("ps:SendCurrentDepartmentID")
@@ -25,7 +27,7 @@ AddEventHandler("onClientResourceStart", function()
     if currentDepartmentID == 2 or currentDepartmentID == 3 then
         isPolice = true
         inService = false
-        
+        hasReceivedDepartmentID = true
     end
 end)
 
@@ -35,3 +37,21 @@ AddEventHandler("ps:GetCurrentDepartmentID", function(departmentID)
     hasReceivedDepartmentID = true
 end) 
 
+-- draw marker for station
+Citizen.CreateThread(function() 
+    while true do 
+        Citizen.Wait(0)
+        if isPolice then
+            DrawServiceMarkers()
+        end
+    end
+end)
+
+function DrawServiceMarkers() 
+    for i, s in ipairs(policeStations) do
+        DrawMarker(1, s.x, s.y, s.z - 1, 
+        0.0, 0.0, 0.0, 0, 0.0, 0.0, 
+        0.75, 0.75, 0.75, 17, 103, 177, 255, 
+        false, true, 2, false, false, false, false)
+    end
+end 
